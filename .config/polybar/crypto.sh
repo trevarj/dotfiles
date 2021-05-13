ce16ae9b61642ad800c1baef56ddb377332ffbcd
Trevor Arjeski <tmarjeski@gmail.com>
2021-05-13 19:38:55 +0300

    bar

--- .config/polybar/crypto.sh
+++ .config/polybar/crypto.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-eth=$(curl -s -X GET "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd" -H "accept: application/json" | jq -r ".ethereum.usd")
-
-printf "ETH %.0f\n" $eth
+query=$(curl -s -X GET "https://api.coingecko.com/api/v3/simple/price?ids=ethereum,cardano&vs_currencies=usd" -H "accept: application/json")
+eth=$(jq -r ".ethereum.usd" <<< $query)
+ada=$(jq -r ".cardano.usd" <<< $query)
+printf "ETH %.0f ADA %.2f\n" $eth $ada
 
