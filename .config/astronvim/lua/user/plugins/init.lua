e88931c6901474844127ae3fe636e0fab50aa8e7
Trevor Arjeski <tmarjeski@gmail.com>
2022-12-15 10:33:44 +0300

    add gitblame line

--- .config/astronvim/lua/user/plugins/init.lua
+++ .config/astronvim/lua/user/plugins/init.lua
@@ -1,28 +1,33 @@
 return {
-  ["sindrets/diffview.nvim"] = {
-    opt = true,
-    setup = function()
-      table.insert(astronvim.git_plugins, "diffview.nvim")
-    end,
-    config = function()
-      require("user.plugins.diffview")
-    end,
-  },
-  ["lvimuser/lsp-inlayhints.nvim"] = {
-    module = "lsp-inlayhints",
-    config = function()
-      require("user.plugins.lsp-inlayhints")
-    end,
-  },
-  ["hrsh7th/cmp-nvim-lsp-signature-help"] = {
-    after = "nvim-cmp",
-    config = function()
-      astronvim.add_user_cmp_source("nvim_lsp_signature_help")
-    end,
-  },
-  ["echasnovski/mini.map"] = {
-    config = function()
-      require("user.plugins.minimap")
-    end,
-  },
+	["sindrets/diffview.nvim"] = {
+		opt = true,
+		setup = function()
+			table.insert(astronvim.git_plugins, "diffview.nvim")
+		end,
+		config = function()
+			require("user.plugins.diffview")
+		end,
+	},
+	["lvimuser/lsp-inlayhints.nvim"] = {
+		module = "lsp-inlayhints",
+		config = function()
+			require("user.plugins.lsp-inlayhints")
+		end,
+	},
+	["hrsh7th/cmp-nvim-lsp-signature-help"] = {
+		after = "nvim-cmp",
+		config = function()
+			astronvim.add_user_cmp_source("nvim_lsp_signature_help")
+		end,
+	},
+	["echasnovski/mini.map"] = {
+		config = function()
+			require("user.plugins.minimap")
+		end,
+	},
+	["f-person/git-blame.nvim"] = {
+		config = function()
+			require("user.plugins.git-blame")
+		end,
+	},
 }
