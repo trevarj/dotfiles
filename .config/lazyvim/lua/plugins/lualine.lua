d373162e49be44bca987b97e20444b633ce2bca1
Trevor Arjeski <tmarjeski@gmail.com>
2023-01-11 17:46:09 +0300

    lazyvim lualine and update

--- .config/lazyvim/lua/plugins/lualine.lua
+++ .config/lazyvim/lua/plugins/lualine.lua
@@ -4,6 +4,10 @@ return {
     -- dependencies = { "nvim-lua/lsp-status.nvim" },
     event = "VeryLazy",
     opts = function(_, opts)
+      opts.sections.lualine_y = {
+        { "progress", separator = "", padding = { left = 1, right = 1 } },
+        { "location", padding = { left = 0, right = 1 } },
+      }
       table.remove(opts.sections.lualine_z)
       -- table.insert(opts.sections.lualine_z, "require('lsp-status').status()")
     end,
