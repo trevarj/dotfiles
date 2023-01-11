19df36a59572fb040554cc33826749af465b5524
Trevor Arjeski <tmarjeski@gmail.com>
2023-01-11 12:19:43 +0300

    lazyvim rust-tools lsp

--- .config/lazyvim/lua/plugins/lsp.lua
+++ .config/lazyvim/lua/plugins/lsp.lua
@@ -3,24 +3,6 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "simrat39/rust-tools.nvim",
-      init = function()
-        require("lazyvim.util").on_attach(function(_, bufnr)
-          local rt = require("rust-tools")
-          -- Hover actions
-          vim.keymap.set("n", "<leader>ch", rt.hover_actions.hover_actions, { desc = "Hover Actions", buffer = bufnr })
-          -- Code action groups
-          vim.keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
-          -- Runnables
-          vim.keymap.set("n", "<leader>ce", rt.runnables.runnables, { desc = "Runnables", buffer = bufnr })
-          -- Expand macros
-          vim.keymap.set(
-            "n",
-            "<leader>cE",
-            rt.expand_macro.expand_macro,
-            { desc = "Expand Macro Recursively", buffer = bufnr }
-          )
-        end)
-      end,
     },
     ---@class PluginLspOpts
     opts = {
@@ -31,7 +13,36 @@ return {
       },
       setup = {
         rust_analyzer = function(_, opts)
-          require("rust-tools").setup({ server = opts })
+          local rt = require("rust-tools")
+          rt.setup({ server = opts })
+
+          require("lazyvim.util").on_attach(function(client, bufnr)
+            if client.name == "rust_analyzer" then
+              -- Hover actions
+              vim.keymap.set(
+                "n",
+                "<leader>ch",
+                rt.hover_actions.hover_actions,
+                { desc = "Hover Actions", buffer = bufnr }
+              )
+              -- Code action groups
+              vim.keymap.set(
+                "n",
+                "<leader>cA",
+                rt.code_action_group.code_action_group,
+                { desc = "Run Rust Code Actions", buffer = bufnr }
+              )
+              -- Runnables
+              vim.keymap.set("n", "<leader>ce", rt.runnables.runnables, { desc = "Runnables", buffer = bufnr })
+              -- Expand macros
+              vim.keymap.set(
+                "n",
+                "<leader>cE",
+                rt.expand_macro.expand_macro,
+                { desc = "Expand Macro Recursively", buffer = bufnr }
+              )
+            end
+          end)
           return true
         end,
       },
