03dd4ff9f9d1f9f553fed7ff9128ec915b569fe7
Trevor Arjeski <tmarjeski@gmail.com>
2023-01-11 13:32:23 +0300

    lazyvim enable some more plugins

--- .config/lazyvim/lua/plugins/disabled.lua
+++ .config/lazyvim/lua/plugins/disabled.lua
@@ -4,7 +4,5 @@ return {
   { "folke/trouble.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
-  { "echasnovski/mini.bufremove", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
-  { "windwp/nvim-spectre", enabled = false },
 }
