e742eb5435909324db6fda60b870bebc3ac02a87
Trevor Arjeski <tmarjeski@gmail.com>
2023-03-28 15:04:53 +0300

    polybar crypto parse handling

--- .config/polybar/crypto.sh
+++ .config/polybar/crypto.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 query=$(curl -s -X GET "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,cardano,crypto-com-chain&vs_currencies=usd" -H "accept: application/json")
-btc=$(jq -r ".bitcoin.usd" <<< $query)
-eth=$(jq -r ".ethereum.usd" <<< $query)
-ada=$(jq -r ".cardano.usd" <<< $query)
-cro=$(jq -r '."crypto-com-chain".usd' <<< $query)
+
+if [ "$1" = "-d" ]; then
+    echo "$query"
+    exit;
+fi
+
+btc=$(jq -rn 'try input.bitcoin.usd catch "0"' <<< $query)
+eth=$(jq -rn 'try input.ethereum.usd catch "0"' <<< $query)
+ada=$(jq -rn 'try input.cardano.usd catch "0"' <<< $query)
+cro=$(jq -rn 'try input."crypto-com-chain".usd catch 0' <<< $query)
 printf " %.0f  %.0f  %.2f  %.2f\n" $btc $eth $ada $cro
 
