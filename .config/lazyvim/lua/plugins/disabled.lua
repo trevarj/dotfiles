81296dab2438c325987f6e774fdac548cd889fc5
Trevor Arjeski <tmarjeski@gmail.com>
2023-04-19 22:30:01 +0300

    [lazyvim] annoying

--- .config/lazyvim/lua/plugins/disabled.lua
+++ .config/lazyvim/lua/plugins/disabled.lua
@@ -7,4 +7,5 @@ return {
   { "echasnovski/mini.indentscope", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
