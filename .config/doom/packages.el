692969f2ab76b4e7f63516af97d6e260995f82ff
Trevor Arjeski <tmarjeski@gmail.com>
2023-06-22 00:46:41 +0300

    [doom] config

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -19,6 +19,9 @@
 ;; (package! ement
 ;;   :recipe (:host github :repo "alphapapa/ement.el"))
 
+;; Evil Lisp'ing
+(package! evil-cleverparens)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
