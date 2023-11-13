c3587b2ad94cf6b0ed14d39ec4c7a21f3ce82683
Trevor Arjeski <tmarjeski@gmail.com>
2023-11-13 11:30:43 +0300

    [lazyvim] lsp n stuff

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -7,6 +7,44 @@ vim.g.nord_contrast = false
 vim.g.nord_italic = false
 vim.g.nord_borders = true
 
+-- Rustacean
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {},
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+    end,
+    settings = {
+      -- rust-analyzer language server configuration
+      ["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
+          loadOutDirsFromCheck = true,
+          runBuildScripts = true,
+        },
+        -- Add clippy lints for Rust.
+        checkOnSave = {
+          allFeatures = false,
+          command = "clippy",
+          extraArgs = { "--no-deps" },
+        },
+        procMacro = {
+          enable = true,
+          ignored = {
+            ["async-trait"] = { "async_trait" },
+            ["napi-derive"] = { "napi" },
+            ["async-recursion"] = { "async_recursion" },
+          },
+        },
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {},
+}
+
 vim.opt.guicursor = "n-v-c:block,i:blinkon1"
 
 -- tildes...
