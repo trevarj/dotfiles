2c07543d260de7f03ba2ad0059c411a69ac3875a
Trevor Arjeski <tmarjeski@gmail.com>
2023-12-19 22:06:41 +0300

    [lazyvim] more colors

--- .config/lazyvim/lua/plugins/ui.lua
+++ .config/lazyvim/lua/plugins/ui.lua
@@ -2,29 +2,31 @@ return {
   {
     "gbprod/nord.nvim",
     dev = false,
-    opts = {
-      transparent = false, -- Enable this to disable setting the background color
-      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-      diff = { mode = "fg" }, -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
-      borders = true, -- Enable the border between verticaly split windows visible
-      errors = { mode = "fg" }, -- Display mode for errors and diagnostics
-      -- values : [bg|fg|none]
-      search = { theme = "vim" }, -- theme for highlighting search results
-      -- values : [vim|vscode]
-      styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = false },
-        keywords = { bold = true },
-        functions = {},
-        variables = {},
+    opts = function(_, _)
+      local colors = require("nord.colors").palette
+      return {
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        diff = { mode = "fg" }, -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
+        borders = true, -- Enable the border between verticaly split windows visible
+        errors = { mode = "fg" }, -- Display mode for errors and diagnostics
+        -- values : [bg|fg|none]
+        search = { theme = "vim" }, -- theme for highlighting search results
+        -- values : [vim|vscode]
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { bold = true },
+          functions = { fg = colors.aurora.orange, bold = true },
+          variables = {},
 
-        -- To customize lualine/bufferline
-        bufferline = {
-          current = {},
-          modified = { italic = false },
+          -- To customize lualine/bufferline
+          bufferline = {
+            current = {},
+            modified = { italic = false },
+          },
         },
-
         -- colorblind mode
         -- see https://github.com/EdenEast/nightfox.nvim#colorblind
         -- simulation mode has not been implemented yet.
@@ -32,19 +34,19 @@ return {
           enable = true,
           preserve_background = true,
           severity = {
-            protan = 0.4,
-            deutan = 1.0,
-            tritan = 0.0,
+            protan = 0.2,
+            deutan = 0.9,
+            tritan = 0.4,
           },
         },
-      },
 
-      --- You can override specific highlights to use other groups or a hex color
-      --- function will be called with all highlights and the colorScheme table
-      on_highlights = function(hl, c)
-        hl.LspInlayHint = { fg = c.frost.artic_ocean }
-      end,
-    },
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with all highlights and the colorScheme table
+        on_highlights = function(hl, c)
+          hl.LspInlayHint = { fg = c.frost.artic_ocean }
+        end,
+      }
+    end,
   },
   {
     "LazyVim/LazyVim",
