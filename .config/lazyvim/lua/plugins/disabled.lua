8fbab03092f9a1a8c02f5087b1c9cfd56c8cc8e0
Trevor Arjeski <tmarjeski@gmail.com>
2023-12-28 22:11:01 +0300

    [lazyvim] replace mini.pairs

--- .config/lazyvim/lua/plugins/disabled.lua
+++ .config/lazyvim/lua/plugins/disabled.lua
@@ -6,6 +6,7 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
   { "williamboman/mason-lspconfig.nvim", enabled = enable },
   { "williamboman/mason.nvim", enabled = enable },
 }
