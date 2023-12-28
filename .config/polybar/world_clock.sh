3536a3f3ffe96efdf221c66e515a170b076d3dce
Trevor Arjeski <tmarjeski@gmail.com>
2023-12-28 22:10:34 +0300

    [nerd font] installed arch package for jetbrains mono

--- .config/polybar/world_clock.sh
+++ .config/polybar/world_clock.sh
@@ -3,25 +3,24 @@ t=0
 sleep_pid=0
 
 toggle() {
-    t=$(((t + 1) % 2))
+	t=$(((t + 1) % 2))
 
-    if [ "$sleep_pid" -ne 0 ]; then
-        kill $sleep_pid >/dev/null 2>&1
-    fi
+	if [ "$sleep_pid" -ne 0 ]; then
+		kill $sleep_pid >/dev/null 2>&1
+	fi
 }
-
 
 trap "toggle" USR1
 
 while true; do
-    if [ $t -eq 0 ]; then
-        echo ""
-    else
+	if [ $t -eq 0 ]; then
+		echo "󰇧"
+	else
 		HK=$(TZ='Asia/Hong_Kong' date +'%-H:%M')
 		NY=$(TZ='America/New_York' date +'%-H:%M')
 		echo "HK $HK NY $NY"
-    fi
-    sleep 1 &
-    sleep_pid=$!
-    wait
+	fi
+	sleep 1 &
+	sleep_pid=$!
+	wait
 done
