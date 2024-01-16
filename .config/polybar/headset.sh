1e66268b9ccfe09c7f4be7320c9c184b184304e0
Trevor Arjeski <tmarjeski@gmail.com>
2024-01-16 10:37:10 +0300

    [polybar/tmux] icons

--- .config/polybar/headset.sh
+++ .config/polybar/headset.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-if bat=$(arctis7-controls battery);
-then
-        bat="${bat#Battery level: *}"
-        bat="${bat%*%}"
-        # bat=$(echo "$bat" | awk '{print $3}' | tr -d %)
-        if [ $bat -eq 0 ]; then
-                echo ""
-        elif [ $bat -le 10 ]; then
-                echo " "
-        elif [ $bat -le 25 ]; then
-                echo " "
-        elif [ $bat -le 50 ]; then
-                echo " "
-        elif [ $bat -le 75 ]; then
-                echo " "
-        else
-                echo " "
-        fi
+if bat=$(arctis7-controls battery); then
+	bat="${bat#Battery level: *}"
+	bat="${bat%*%}"
+	# bat=$(echo "$bat" | awk '{print $3}' | tr -d %)
+	if [ $bat -eq 0 ]; then
+		echo ""
+	elif [ $bat -le 10 ]; then
+		echo "󰋎 "
+	elif [ $bat -le 25 ]; then
+		echo "󰋎 "
+	elif [ $bat -le 50 ]; then
+		echo "󰋎 "
+	elif [ $bat -le 75 ]; then
+		echo "󰋎 "
+	else
+		echo "󰋎 "
+	fi
 fi
