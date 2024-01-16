fae66f16dcad7ab7c723f44055b253f396ae8e89
Trevor Arjeski <tmarjeski@gmail.com>
2024-01-16 17:52:35 +0300

    [polybar] headset AGAIN

--- .config/polybar/headset.sh
+++ .config/polybar/headset.sh
@@ -6,6 +6,8 @@ len=${#icons[@]}
 if bat=$(arctis7-controls battery); then
   bat="${bat#Battery level: *}"
   bat="${bat%*%}"
-  idx=$(( $bat % $len ))
-  echo "󰋎 ${icons[idx]}"
+  if [ $bat -ne 0 ]; then
+    idx=$(( $bat % ($len + 1) ))
+    echo "󰋎 ${icons[idx]}"
+  fi
 fi
