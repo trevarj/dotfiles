f5918d0ffbd7e24807e36e589410e5bf953b00fb
Trevor Arjeski <tmarjeski@gmail.com>
2024-01-17 13:40:51 +0300

    remove lazyvim

--- .config/lazyvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up" })
-
-vim.keymap.set("n", "<leader>bc", "<cmd>%bd|e#|bd#<cr><cr>", { desc = "Close all buffers except current" })
