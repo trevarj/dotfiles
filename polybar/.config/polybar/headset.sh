711a07cf0e86e0eca61c594f086324f7a2aa044a
Trevor Arjeski <tmarjeski@gmail.com>
2024-02-01 09:05:08 +0300

    [polybar] clean headset script

--- polybar/.config/polybar/headset.sh
+++ polybar/.config/polybar/headset.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # icons=("󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
 icons=("" "" "" "" "")
 len=${#icons[@]}
-perc=$(( 100 / ($len - 1) ))
+perc=$((100 / (len - 1)))
 if bat=$(arctis7-controls battery); then
-  bat="${bat#Battery level: *}"
-  bat="${bat%*%}"
-  if [ $bat -ne 0 ]; then
-    idx=$(( (($bat + $perc - 1) / $perc) % $len ))
-    echo "󰋎 ${icons[idx]}"
-  else 
-    echo ""
-  fi
+	bat="${bat#Battery level: *}"
+	bat="${bat%*%}"
+	if [ "$bat" -ne 0 ]; then
+		idx=$((((bat + perc - 1) / perc) % len))
+		echo "󰋎 ${icons[idx]}"
+	else
+		echo ""
+	fi
 fi
