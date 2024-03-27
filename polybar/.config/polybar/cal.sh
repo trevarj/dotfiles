5d05edad3d2162673ba0c5c4cce97f6d9428c367
Trevor Arjeski <tmarjeski@gmail.com>
2024-03-27 12:10:52 +0300

    [polybar] improve cal popup with controls

--- polybar/.config/polybar/cal.sh
+++ polybar/.config/polybar/cal.sh
@@ -1,11 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "󰃭"
+
+launch_cal='
+	i=0
+	while true; do
+    clear
+		cal -m -- $offset
+    read -rsn1 key
+		case "$key" in
+		n)
+      ((i++))
+			;;
+		p)
+      ((i--))
+			;;
+		esac
+    if [ "$i" -gt 0 ]; then
+      offset="+${i}month"
+    elif [ "$i" -lt 0 ]; then
+      offset="${i}month"
+    else
+      offset=
+    fi
+	done
+}
+'
+
 case "$1" in
 --toggle)
 	if ! pgrep cal.sh | grep -v "$$" | xargs -n1 pkill -P; then
-		kitty --name cal --hold sh -c "tput smcup; tput civis; cal; read -r -s password;"
+		kitty --name cal --hold sh -c "tput civis; $launch_cal;"
 	fi
 	;;
-*) ;;
 esac
