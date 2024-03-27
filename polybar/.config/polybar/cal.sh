8b00a9795cd07c31c5e33ee4b7d7221a28d837c9
Trevor Arjeski <tmarjeski@gmail.com>
2024-03-27 12:22:20 +0300

    [polybar] cal declare function

--- polybar/.config/polybar/cal.sh
+++ polybar/.config/polybar/cal.sh
@@ -2,35 +2,33 @@
 
 echo "󰃭"
 
-launch_cal='
+launch_cal() {
 	i=0
 	while true; do
-    clear
+		clear
+		# shellcheck disable=SC2086
 		cal -m -- $offset
-    read -rsn1 key
+		read -rsn1 key
 		case "$key" in
 		n)
-      ((i++))
+			((i++))
 			;;
 		p)
-      ((i--))
+			((i--))
 			;;
 		esac
-    if [ "$i" -gt 0 ]; then
-      offset="+${i}month"
-    elif [ "$i" -lt 0 ]; then
-      offset="${i}month"
-    else
-      offset=
-    fi
+		if [ "$i" -gt 0 ]; then
+			offset="+${i}month"
+		elif [ "$i" -lt 0 ]; then
+			offset="${i}month"
+		fi
 	done
 }
-'
 
 case "$1" in
 --toggle)
 	if ! pgrep cal.sh | grep -v "$$" | xargs -n1 pkill -P; then
-		kitty --name cal --hold sh -c "tput civis; $launch_cal;"
+		kitty --name cal --hold bash -c "$(declare -f launch_cal); tput civis; launch_cal;"
 	fi
 	;;
 esac
