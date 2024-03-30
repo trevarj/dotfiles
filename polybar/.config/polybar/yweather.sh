f383df68a0685f32c8fc9413e94a68e97b0150d7
Trevor Arjeski <tmarjeski@gmail.com>
2024-03-30 20:22:24 +0300

    [polybar] use shell script for weather instead of rust

--- polybar/.config/polybar/yweather.sh
+++ polybar/.config/polybar/yweather.sh
@@ -1,4 +1,34 @@
 #!/bin/sh
 
 . "$HOME/.dotenv"
-yweather --lat "$YW_LAT" --long "$YW_LONG" --key ca39a767-93d5-42eb-bf6b-84bb5e815d35 2>/dev/null || echo "N/A"
+
+out() {
+	printf "%s %d°" "$1" "$temp"
+}
+
+if
+	res=$(curl -s -X GET \
+		-H "Access-Control-Allow-Headers: X-Yandex-API-Key" \
+		-H "X-Yandex-API-Key: $YW_API_KEY" \
+		"https://api.weather.yandex.ru/v2/informers?lat=$YW_LAT&lon=$YW_LONG")
+then
+	temp=$(echo "$res" | jq -rn 'try input.fact.temp catch "N/A"')
+	condition=$(echo "$res" | jq -rn 'try input.fact.condition catch ""')
+	case "$condition" in
+	"clear") out "󰖙" ;;
+	"partly-cloudy") out "󰖕" ;;
+	"cloudy" | "overcast") out "󰖐" ;;
+	"drizzle" | "light-rain" | "rain" | "moderate-rain") out "󰖗" ;;
+	"showers" | "heavy-rain" | "continuous-heavy-rain") out "󰖖" ;;
+	"wet-snow") out "󰙿" ;;
+	"light-snow") out "󰖘" ;;
+	"snow" | "snow-showers") out "󰼶" ;;
+	"hail") out "󰖒" ;;
+	"thunderstorm") out "󰖓" ;;
+	"thunderstorm-with-rain" | "thunderstorm-with-hail") out "󰙾" ;;
+	*) out "-" ;;
+	esac
+else
+	temp="N/A"
+	out "-"
+fi
