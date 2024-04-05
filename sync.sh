77f66f25ac8e5a470b9223c011760c1ca938f1ea
Trevor Arjeski <tmarjeski@gmail.com>
2024-04-05 09:34:46 +0300

    [sync.sh] fix adding userChrome

--- sync.sh
+++ sync.sh
@@ -10,7 +10,7 @@ packages=$(fd -t d -d 1 -E _untracked | sed 's/^\.\///; s/\/$//')
 for p in $packages; do
 	if [ "$p" = "firefox" ]; then
 		# firefox profiles are not consistantly named, only a guess
-		profile="$HOME/.mozilla/firefox/*default-release-*/chrome"
+		profile="$HOME/.mozilla/firefox/*default-release*/chrome"
 		# shellcheck disable=SC2086 # intentionally glob the path
 		stow -t $profile "$cmd" "$p"
 	else
