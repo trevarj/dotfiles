d053d962afa7527842d734ade71b86bcfbd1b3d3
Trevor Arjeski <tmarjeski@gmail.com>
2024-04-08 00:14:44 +0300

    [polybar] get emerge updates from cron created file

--- polybar/.config/polybar/updates.sh
+++ polybar/.config/polybar/updates.sh
@@ -2,28 +2,32 @@
 # Check for updates on Arch or Gentoo
 
 if [ -e /etc/gentoo-release ]; then
-  is_gentoo=1
+	is_gentoo=1
 fi
 
 case "$1" in
-  run)
-    if [ -n "$is_gentoo" ]; then
-      sudo emerge --ask --deep --update @world
-    else
-      sudo pacman -Syu
-    fi
-    ;;
-  *)
-    if [ -n "$is_gentoo" ]; then
-      updates=$(emerge -DNuvp --quiet world | wc -l) 
-    else
-      updates=$(checkupdates 2>/dev/null | wc -l)
-    fi
+run)
+	if [ -n "$is_gentoo" ]; then
+		sudo emerge --ask --deep --update @world
+	else
+		sudo pacman -Syu
+	fi
+	;;
+*)
+	if [ -n "$is_gentoo" ]; then
+		if [ -f "/tmp/updates.emerge" ]; then
+			updates=$(cat "/tmp/updates.emerge")
+		else
+			updates=$(emerge -DNuvp --quiet world | wc -l)
+		fi
+	else
+		updates=$(checkupdates 2>/dev/null | wc -l)
+	fi
 
-    if [ $((updates)) -gt 0 ]; then
-      echo "󰏗 $updates"
-    else
-      echo ""
-    fi
-    ;; 
-esac 
+	if [ $((updates)) -gt 0 ]; then
+		echo "󰏗 $updates"
+	else
+		echo ""
+	fi
+	;;
+esac
