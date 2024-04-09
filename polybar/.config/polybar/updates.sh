0e89f1d03f58cdd7193ee381ce8b08ac3f8b4187
Trevor Arjeski <tmarjeski@gmail.com>
2024-04-09 20:06:11 +0300

    [polybar] gentoo update only expect cron-made file

--- polybar/.config/polybar/updates.sh
+++ polybar/.config/polybar/updates.sh
@@ -3,22 +3,25 @@
 
 if [ -e /etc/gentoo-release ]; then
 	is_gentoo=1
+	emerge_update="/tmp/updates.emerge"
 fi
 
 case "$1" in
 run)
 	if [ -n "$is_gentoo" ]; then
-		sudo emerge --ask --deep --update @world
+		if sudo emerge --ask --deep --update @world; then
+			sudo rm "$emerge_update"
+			sudo emerge --ask --depclean
+		fi
 	else
 		sudo pacman -Syu
 	fi
 	;;
 *)
 	if [ -n "$is_gentoo" ]; then
-		if [ -f "/tmp/updates.emerge" ]; then
-			updates=$(cat "/tmp/updates.emerge")
-		else
-			updates=$(emerge -DNuvp --quiet world | wc -l)
+		# expects a file with the result of `emerge -DNuvp --quiet world | wc -l`
+		if [ -f "$emerge_update" ]; then
+			updates=$(cat "$emerge_update")
 		fi
 	else
 		updates=$(checkupdates 2>/dev/null | wc -l)
