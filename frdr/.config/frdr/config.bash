db626658375255a5d2eec3142605287e5d15d437
Trevor Arjeski <tmarjeski@gmail.com>
2024-04-28 09:03:23 +0300

    [frdr] init

---
+++ frdr/.config/frdr/config.bash
@@ -0,0 +1,33 @@
+# shellcheck disable=SC2034
+# FEEDS=(
+#   # ["alias"]="http://example.com/rss.xml"
+#   # "local /tmp/rss.xml"
+#   # "local_atom /tmp/feed.xml"
+# )
+
+# Keeping my feeds private
+source ./feeds.bash
+
+FRDR_PAGER="w3m -T text/html confirm_qq=false"
+FRDR_PREVIEWER="w3m -T text/html"
+FRDR_SHOW_HELP=true
+
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+grey=$(tput setaf 240)
+lime_yellow=$(tput setaf 190)
+powder_blue=$(tput setaf 153)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+bold=$(tput bold)
+dim=$(tput dim)
+normal=$(tput sgr0)
+blink=$(tput blink)
+reverse=$(tput smso)
+underline=$(tput smul)
+
+FRDR_POST_FMT=${cyan}${dim}"%(%d %b %y %k:%M)T${normal}  ${green}${bold}%12s${normal} ${cyan}${dim}│${normal} ${cyan}%s${normal}"
