de515c12d15b5b8351eb0082674d1caaefc829a9
Trevor Arjeski <tmarjeski@gmail.com>
2024-05-30 13:32:25 +0300

    [guix] round 2

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -1,10 +1,22 @@
-(cons* (channel
+(list (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
-        ;; Enable signature verification:
+        (branch "master")
+        (commit
+          "34935b49ae20d236b0fc89943f0b1d0d5dea3a33")
         (introduction
-         (make-channel-introduction
-          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-          (openpgp-fingerprint
-           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-       %default-channels)
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "d7a65f1e910aa41684d4cff62014312420848341")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
