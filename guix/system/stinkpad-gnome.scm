e9c57901aba28945d58785cc9064ad4a232527df
Trevor Arjeski <tmarjeski@gmail.com>
2024-06-01 23:28:53 +0300

    [guix] gnome

--- guix/system/stinkpad-gnome.scm
+++ guix/system/stinkpad-gnome.scm
@@ -4,38 +4,29 @@
              (nongnu system linux-initrd))
 
 (use-service-modules
-  admin
-  audio
-  avahi
   cups
-  dbus
   desktop
   guix
   linux
-  networking
-  pm
   ssh
-  sysctl
   xorg)
  
 (use-package-modules
   audio
   bash
-  certs
   cups
   emacs
   file-systems
   fonts
-  freedesktop
+  gnome
   libusb
   linux
-  networking
   package-management
   shells
   ssh
   version-control
-  video
-  wm)
+  video)
+
  
 (operating-system
   (host-name "stinkpad")
@@ -105,55 +96,35 @@
 
   ;; Base packages, others will be installed in using a manifest
   (packages (cons* brightnessctl
+                   emacs-pgtk
                    exfat-utils
                    fuse-exfat
                    git
-                   sway
-                   swaylock
                    zsh
                    %base-packages))
 
   ;; Configure only the services necessary to run the system
   (services
    (append
-    (modify-services %base-services
-     (delete login-service-type)
-     (delete mingetty-service-type)
-     (delete console-font-service-type))
+    (modify-services %desktop-services
+                     ;; Use Wayland
+                     (gdm-service-type
+                      config =>
+                      (gdm-configuration
+                       (inherit config)
+                       (wayland? #t)))
+                     ;; Configure TTYs
+                     (console-font-service-type
+                      config =>
+                      (map (lambda (tty)
+                             ;; Use a larger font for HIDPI screens
+                             (cons tty (file-append
+                                        font-terminus
+                                        "/share/consolefonts/ter-128n")))
+                           '("tty1" "tty2" "tty3"))))
     (list
-     ;; Seat management (can't use seatd because Wireplumber depends on elogind)
-     (service elogind-service-type)
-
-     ;; Configure TTYs and graphical greeter
-     (service console-font-service-type
-       (map (lambda (tty)
-              ;; Use a larger font for HIDPI screens
-              (cons tty (file-append
-                         font-terminus
-                         "/share/consolefonts/ter-128n")))
-            '("tty1" "tty2" "tty3")))
-
-     (service greetd-service-type
-              (greetd-configuration
-               (greeter-supplementary-groups (list "video" "input"))
-               (terminals
-                (list
-                 ;; TTY1 is the graphical login screen for Sway
-                 (greetd-terminal-configuration
-                  (terminal-vt "1")
-                  (terminal-switch #t))
-                 ;; Set up remaining TTYs for terminal use
-                 (greetd-terminal-configuration (terminal-vt "2"))
-                 (greetd-terminal-configuration (terminal-vt "3"))))))
-
-     ;; Configure swaylock as a setuid program
-     (service screen-locker-service-type
-              (screen-locker-configuration
-               (name "swaylock")
-               (program (file-append swaylock "/bin/swaylock"))
-               (using-pam? #t)
-               (using-setuid? #f)))
-
+     (service gnome-desktop-service-type)
+    
      ;; Configure the Guix service and ensure we use Nonguix substitutes
      (simple-service 'add-nonguix-substitutes
                      guix-service-type
@@ -167,36 +138,6 @@
                        (append (list (plain-file "nonguix.pub"
                                                  "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
                                %default-authorized-guix-keys))))
-
-     ;; Set up Polkit to allow `wheel' users to run admin tasks
-     polkit-wheel-service
-
-     ;; Networking services
-     (service network-manager-service-type
-              (network-manager-configuration))
-
-     (service wpa-supplicant-service-type) ;; Needed by NetworkManager
-     (service bluetooth-service-type
-              (bluetooth-configuration
-               (auto-enable? #t)))
-     (service usb-modeswitch-service-type)
-
-     ;; Basic desktop system services (copied from %desktop-services)
-     (service avahi-service-type)
-     (service udisks-service-type)
-     (service upower-service-type)
-     (service cups-pk-helper-service-type)
-     (service geoclue-service-type)
-     (service polkit-service-type)
-     (service dbus-root-service-type)
-     fontconfig-file-system-service ;; Manage the fontconfig cache
-
-     ;; Power and thermal management services
-     (service thermald-service-type)
-     (service tlp-service-type
-              (tlp-configuration
-               (cpu-boost-on-ac? #t)
-               (wifi-pwr-on-bat? #t)))
 
      ;; Enable SSH access
      (service openssh-service-type
@@ -213,15 +154,7 @@
                 (list cups-filters
                       epson-inkjet-printer-escpr))))
 
-     ;; Set up the X11 socket directory for XWayland
-     (service x11-socket-directory-service-type)
-
-     ;; Sync system clock with time servers
-     (service ntp-service-type)
-
-     ;; Add udev rules for MTP (mobile) devices for non-root user access
-     (simple-service 'mtp udev-service-type (list libmtp))
-
      ;; Add udev rules for a few packages
      (udev-rules-service 'pipewire-add-udev-rules pipewire)
      (udev-rules-service 'brightnessctl-udev-rules brightnessctl)))))
+
