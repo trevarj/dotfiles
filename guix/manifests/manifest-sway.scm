3d99920c7369970e7f12189e57e1ef3bdcf28c44
Trevor Arjeski <tmarjeski@gmail.com>
2024-06-02 13:16:43 +0300

    [guix] initial sway manifest

---
+++ guix/manifests/manifest-sway.scm
@@ -0,0 +1,35 @@
+;; This "manifest" file can be passed to 'guix package -m' to reproduce
+;; the content of your profile.  This is "symbolic": it only specifies
+;; package names.  To reproduce the exact same profile, you also need to
+;; capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(specifications->manifest
+  (list "pinentry-tty"
+        "flatpak-xdg-utils"
+        "wireplumber"
+        "emacs-pgtk"
+        "font-terminus"
+        "ncurses"
+        "ripgrep"
+        "fd"
+        "gammastep"
+        "pavucontrol"
+        "dbus"
+        "weechat"
+        "flatpak"
+        "libtool"
+        "libvterm"
+        "jq"
+        "grimshot"
+        "mpv"
+        "fzf"
+        "zsh-autopair"
+        "zsh-syntax-highlighting"
+        "zsh-autosuggestions"
+        "zsh-completions"
+        "zsh"
+        "gnupg"
+        "kitty"
+        "curl"
+        "stow"))
