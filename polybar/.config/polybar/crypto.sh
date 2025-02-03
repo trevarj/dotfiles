2dfbd35bf0873d65da9a2e0982ff7eb238428d2c
Trevor Arjeski <tmarjeski@gmail.com>
2025-02-03 21:27:46 +0300

    random tweaking for gnome

--- polybar/.config/polybar/crypto.sh
+++ polybar/.config/polybar/crypto.sh
@@ -3,13 +3,17 @@
 query=$(curl -s -X GET "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,cardano,crypto-com-chain&vs_currencies=usd" -H "accept: application/json")
 
 if [ "$1" = "-d" ]; then
-    echo "$query"
-    exit;
+  echo "$query"
+  exit
 fi
 
 btc=$(echo "$query" | jq -rn 'try input.bitcoin.usd catch "0"')
 eth=$(echo "$query" | jq -rn 'try input.ethereum.usd catch "0"')
 ada=$(echo "$query" | jq -rn 'try input.cardano.usd catch "0"')
 cro=$(echo "$query" | jq -rn 'try input."crypto-com-chain".usd catch 0')
-printf "%%{T4} %%{T-}%.0f %%{T4} %%{T-}%.0f %%{T4} %%{T-}%.2f %%{T4} %%{T-}%.2f\n" "$btc" "$eth" "$ada" "$cro"
 
+if [ "$1" = "-r" ]; then
+  printf " %.0f  %.0f  %.2f  %.2f\n" "$btc" "$eth" "$ada" "$cro"
+else
+  printf "%%{T4} %%{T-}%.0f %%{T4} %%{T-}%.0f %%{T4} %%{T-}%.2f %%{T4} %%{T-}%.2f\n" "$btc" "$eth" "$ada" "$cro"
+fi
