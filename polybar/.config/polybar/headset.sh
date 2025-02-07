946a49bf1926e5b86d7af5f53d8b58fc05844104
Trevor Arjeski <tmarjeski@gmail.com>
2025-02-07 14:23:21 +0300

    [polybar] script hardcode spacing

--- polybar/.config/polybar/headset.sh
+++ polybar/.config/polybar/headset.sh
@@ -5,12 +5,12 @@ icons=("" "" "" "" "")
 len=${#icons[@]}
 perc=$((100 / (len - 1)))
 if bat=$(arctis7-controls battery); then
-	bat="${bat#Battery level: *}"
-	bat="${bat%*%}"
-	if [ "$bat" -ne 0 ]; then
-		idx=$((((bat + perc - 1) / perc) % len))
-		echo "󰋎 ${icons[idx]}"
-	else
-		echo ""
-	fi
+  bat="${bat#Battery level: *}"
+  bat="${bat%*%}"
+  if [ "$bat" -ne 0 ]; then
+    idx=$((((bat + perc - 1) / perc) % len))
+    echo "󰋎 ${icons[idx]} "
+  else
+    echo ""
+  fi
 fi
--- polybar/.config/polybar/notification-github.sh
+++ polybar/.config/polybar/notification-github.sh
@@ -8,7 +8,7 @@ TOKEN="$GH_TOKEN"
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
-	echo " $notifications"
+  echo " $notifications  "
 else
-	echo ""
+  echo ""
 fi
