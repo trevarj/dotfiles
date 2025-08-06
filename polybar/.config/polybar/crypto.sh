452284b83b082be65ac2ba3dd650c2b080541b2e
Trevor Arjeski <tmarjeski@gmail.com>
2025-08-06 11:55:18 +0300

    remove coins

--- polybar/.config/polybar/crypto.sh
+++ polybar/.config/polybar/crypto.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-query=$(curl -s -X GET "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,cardano,crypto-com-chain&vs_currencies=usd" -H "accept: application/json")
+query=$(curl -s -X GET "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd" -H "accept: application/json")
 
 if [ "$1" = "-d" ]; then
   echo "$query"
@@ -9,11 +9,9 @@ fi
 
 btc=$(echo "$query" | jq -rn 'try input.bitcoin.usd catch "0"')
 eth=$(echo "$query" | jq -rn 'try input.ethereum.usd catch "0"')
-ada=$(echo "$query" | jq -rn 'try input.cardano.usd catch "0"')
-cro=$(echo "$query" | jq -rn 'try input."crypto-com-chain".usd catch 0')
 
 if [ "$1" = "-r" ]; then
-  printf " %.0f  %.0f  %.2f  %.2f\n" "$btc" "$eth" "$ada" "$cro"
+  printf " %.0f  %.0f\n" "$btc" "$eth"
 else
-  printf "%%{T4} %%{T-}%.0f %%{T4} %%{T-}%.0f %%{T4} %%{T-}%.2f %%{T4} %%{T-}%.2f\n" "$btc" "$eth" "$ada" "$cro"
+  printf "%%{T4} %%{T-}%.0f %%{T4} %%{T-}%.0f\n" "$btc" "$eth"
 fi
