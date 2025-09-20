f56d630691a1b26adddc55275e02cd35abfdde88
Trevor Arjeski <tmarjeski@gmail.com>
2025-09-20 21:37:44 +0300

    [guix] reorg channels

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -1,20 +1,21 @@
-(list (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
-        ;; (commit
-        ;;   "34935b49ae20d236b0fc89943f0b1d0d5dea3a33")
-        (introduction
-          (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-            (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-      (channel
-        (name 'guix)
-        (url "https://codeberg.org/guix/guix.git")
-        (branch "master")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+(list
+ (channel
+  (name 'guix)
+  (url "https://codeberg.org/guix/guix.git")
+  (branch "master")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D E643 A2A0 6DF2 A33A 54FA"))))
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
+  (branch "master")
+  ;; (commit
+  ;;   "34935b49ae20d236b0fc89943f0b1d0d5dea3a33")
+  (introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5")))))
