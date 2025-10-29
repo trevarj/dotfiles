cbec790fd336caa2dc4f9def17d3fc21f3d74045
Trevor Arjeski <tmarjeski@gmail.com>
2025-10-29 16:27:31 +0300

    [guix] flatpak workaround again

--- trev-guix/services/flatpak.scm
+++ trev-guix/services/flatpak.scm
@@ -15,8 +15,10 @@
 ;; workaround to get dbus to load flatpak services because flatpak.sh gets
 ;; run too late
 (define (home-flatpak-environment-variables _)
-  '(("XDG_DATA_DIRS" .
-     "${XDG_DATA_HOME}share/flatpak/exports/share:${XDG_DATA_DIRS}")))
+  `(("XDG_DATA_DIRS" .
+     ,(string-append (getenv "XDG_DATA_HOME")
+                     "share/flatpak/exports/share:"
+                     (getenv "XDG_DATA_DIRS")))))
 
 (define-public home-flatpak-service-type
   (service-type
