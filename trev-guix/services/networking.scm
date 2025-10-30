962736e6f4b300d6adcf966b71c9d80ece96523a
Trevor Arjeski <tmarjeski@gmail.com>
2025-10-30 14:13:54 +0300

    [guix] refactor networking services

--- trev-guix/services/networking.scm
+++ trev-guix/services/networking.scm
@@ -2,46 +2,40 @@
   #:use-module (gnu services)
   #:use-module (gnu home services shepherd)
   #:use-module (guix gexp)
+  #:use-module (ice-9 match)
   #:use-module (ice-9 rdelim)
   #:use-module (trev-guix packages networking))
 
-(define-public (byedpi-home-shepherd-service args)
-  (list
-   (shepherd-service
-    (provision '(byedpi))
-    (modules '((shepherd support))) ; For %user-log-dir
-    (start #~(make-forkexec-constructor
-              (cons* "ciadpi" '#$args)
-              #:log-file (string-append %user-log-dir "/byedpi.log")))
-    (stop #~(make-kill-destructor)))))
+(define simple-daemon-home-shepherd-service
+  (match-lambda
+    ((pkg exe config-file)
+     (list
+      (let ((args (with-input-from-file config-file
+                    (lambda () (string-split (string-trim (read-line)) #\space)))))
+        (shepherd-service
+          (provision `(,pkg))
+          (modules '((shepherd support))) ; For %user-log-dir
+          (start #~(make-forkexec-constructor
+                    (cons* #$exe '#$args)
+                    #:log-file (string-append %user-log-dir "/" #$exe ".log")))
+          (stop #~(make-kill-destructor))))))))
+
+(define (simple-daemon-service-type pkg exe)
+  (service-type
+    (name (symbol-append pkg '-service))
+    (extensions
+     (list (service-extension
+            home-shepherd-service-type
+            simple-daemon-home-shepherd-service)))
+    (default-value (list pkg exe
+                         (string-append
+                          (getenv "XDG_CONFIG_HOME") "/"
+                          (symbol->string pkg)
+                          "/args.conf")))
+    (description (format #f "Run ~a as a user daemon." pkg))))
 
 (define-public byedpi-service-type
-  (service-type
-    (name 'byedpi-service)
-    (extensions
-     (list (service-extension home-shepherd-service-type
-                              byedpi-home-shepherd-service)))
-    (description "Run byedpi as a user daemon.")))
-
-(define-public (wstunnel-home-shepherd-service config-file)
-  (let ((args (with-input-from-file (car config-file)
-                (lambda ()
-                  (string-split (string-trim (read-line)) #\space)))))
-    (list
-     (shepherd-service
-       (provision '(wstunnel))
-       (modules '((shepherd support)))  ; For %user-log-dir
-       (start #~(make-forkexec-constructor
-                 (cons* "wstunnel" '#$args)
-                 #:log-file (string-append %user-log-dir "/wstunnel.log")))
-       (stop #~(make-kill-destructor))))))
+  (simple-daemon-service-type 'byedpi "ciadpi"))
 
 (define-public wstunnel-service-type
-  (service-type
-    (name 'wstunnel-service)
-    (extensions
-     (list (service-extension home-shepherd-service-type
-                              wstunnel-home-shepherd-service)))
-    (default-value `(,(string-append (getenv "XDG_CONFIG_HOME")
-                                     "/wstunnel/args.conf")))
-    (description "Run wstunnel as a user daemon.")))
+  (simple-daemon-service-type 'wstunnel "wstunnel"))
