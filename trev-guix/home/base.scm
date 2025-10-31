f50d6ab0014b5019ca44886b1f191c525cb489c4
Trevor Arjeski <tmarjeski@gmail.com>
2025-10-31 13:20:00 +0300

    [guix] fix stupid noisy unbound variable error on guix commands

--- trev-guix/home/base.scm
+++ trev-guix/home/base.scm
@@ -22,71 +22,71 @@
  package-management pretty-print rust-apps shells shellutils ssh
  terminals tls tor version-control video vim vpn web xdisorg)
 
-(define-public %trev-home-base-packages
+(define-public (trev-home-base-packages)
   (list
-      aspell
-      aspell-dict-en
-      aspell-dict-ru
-      byedpi
-      curl
-      direnv
-      distrobox
-      ddcutil
-      emacs-next-next-pgtk
-      (list emacs-next-next-pgtk "doc")
-      eza
-      fd
-      flatpak
-      font-cryptofont
-      font-google-noto
-      font-google-noto-emoji
-      font-google-noto-sans-cjk
-      font-iosevka-jbm
-      font-iotrevka
-      font-nerd-fonts-symbols
-      font-terminus
-      fzf
-      fzf-tab
-      fwupd-nonfree
-      git
-      (specification->package+output "git:send-email")
-      (list glib "bin")
-      gnupg
-      guile-next
-      headsetcontrol-3.1.0
-      jq
-      kitty
-      mpv
-      msmtp
-      neofetch
-      netcat
-      papirus-icon-theme
-      pinentry-tty
-      ripgrep
-      stow
-      torsocks
-      wireguard-tools
-      wstunnel
-      xdg-utils
-      zsh
-      zsh-autopair
-      zsh-autosuggestions
-      zsh-completions
-      zsh-syntax-highlighting))
+   aspell
+   aspell-dict-en
+   aspell-dict-ru
+   byedpi
+   curl
+   direnv
+   distrobox
+   ddcutil
+   emacs-next-next-pgtk
+   (list emacs-next-next-pgtk "doc")
+   eza
+   fd
+   flatpak
+   font-cryptofont
+   font-google-noto
+   font-google-noto-emoji
+   font-google-noto-sans-cjk
+   font-iosevka-jbm
+   font-iotrevka
+   font-nerd-fonts-symbols
+   font-terminus
+   fzf
+   fzf-tab
+   fwupd-nonfree
+   git
+   (specification->package+output "git:send-email")
+   (list glib "bin")
+   gnupg
+   guile-next
+   headsetcontrol-3.1.0
+   jq
+   kitty
+   mpv
+   msmtp
+   neofetch
+   netcat
+   papirus-icon-theme
+   pinentry-tty
+   ripgrep
+   stow
+   torsocks
+   wireguard-tools
+   wstunnel
+   xdg-utils
+   zsh
+   zsh-autopair
+   zsh-autosuggestions
+   zsh-completions
+   zsh-syntax-highlighting))
 
-(define-public %trev-home-base-services
+(define-public (trev-home-base-services)
   (list
-      (service home-dbus-service-type)
-      (service home-pipewire-service-type)
-      (service home-zsh-service-type
-               (home-zsh-configuration
-                 (xdg-flavor? #f)
-                 (zshenv (list (local-file "../../zsh/.zshenv" "zshenv")))
-                 (zshrc (list (local-file "../../zsh/.zshrc" "zshrc")))
-                 (zprofile (list (local-file "../../zsh/.zprofile" "zprofile")))
-                 (environment-variables
-                  '(("ASPELL_DICT_DIR" . "${HOME}/.guix-home/profile/lib/aspell")))))
-      (service home-flatpak-service-type)
-      (service byedpi-service-type)
-      (service wstunnel-service-type)
-      %home-fontconfig-service-extension))
+   (service home-dbus-service-type)
+   (service home-pipewire-service-type)
+   (service home-zsh-service-type
+            (home-zsh-configuration
+              (xdg-flavor? #f)
+              (zshenv (list (local-file "../../zsh/.zshenv" "zshenv")))
+              (zshrc (list (local-file "../../zsh/.zshrc" "zshrc")))
+              (zprofile (list (local-file "../../zsh/.zprofile" "zprofile")))
+              (environment-variables
+               '(("ASPELL_DICT_DIR" . "${HOME}/.guix-home/profile/lib/aspell")))))
+   (service home-flatpak-service-type)
+   (service byedpi-service-type)
+   (service wstunnel-service-type)
+   %home-fontconfig-service-extension))
