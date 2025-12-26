85ef83c3dc5f3f03deaf5938bbf7aa1e6ba4dc41
Trevor Arjeski <tmarjeski@gmail.com>
2025-12-26 11:03:35 +0300

    [niri] notification fixing

--- waybar/.config/waybar/scripts/notifications.sh
+++ waybar/.config/waybar/scripts/notifications.sh
@@ -7,15 +7,17 @@ _status() {
             paused="$(dunstctl is-paused)"
             count="$(dunstctl count waiting)"
             tooltip=""
+            icon=""
             if [ "$paused" == 'true' ]; then
-                icon="󰂛"
-            else
-                icon="󰂚"
+                icon="󰪑"
+            # else
+            #     icon="󰂜"
             fi
             if [ "$count" != '0' ]; then
+                icon="󰅸"
                 tooltip="$count notification(s) awaiting"
             fi
-            echo "{\"text\": \"$icon \", \"tooltip\": \"$tooltip\"}"
+            echo "{\"text\": \"$icon\", \"tooltip\": \"$tooltip\"}"
         done
 }
 
