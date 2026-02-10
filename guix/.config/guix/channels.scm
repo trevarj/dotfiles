6e054a5199a10a9f9ca2fa54b3a394ce61f59c83
Trevor Arjeski <tmarjeski@gmail.com>
2026-02-10 10:25:22 +0300

    [guix] add rustup channel

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -18,4 +18,12 @@
    (make-channel-introduction
     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
     (openpgp-fingerprint
-     "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5")))))
+     "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))
+ (channel
+    (name 'rustup)
+    (url "https://codeberg.org/declantsien/guix-rustup")
+    (introduction
+     (make-channel-introduction
+      "325d3e2859d482c16da21eb07f2c6ff9c6c72a80"
+      (openpgp-fingerprint
+       "F695 F39E C625 E081 33B5  759F 0FC6 8703 75EF E2F5")))))
