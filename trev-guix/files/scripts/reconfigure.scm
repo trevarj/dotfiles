93c685168772944f4a5e1d221041895a527eedd6
Trevor Arjeski <tmarjeski@gmail.com>
2026-02-25 17:42:39 +0000

    [guix] reconfigure script support build flags

--- trev-guix/files/scripts/reconfigure.scm
+++ trev-guix/files/scripts/reconfigure.scm
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 exec guix repl -L "/home/trev/Workspace/dotfiles" -- "$0" "$@"
 !#
-;;; Meant to be installed as a script into a home enviroment
+;;; Meant to be installed as a script into a home environment
 ;;; and then can be called like:
-;;; $ guix-reconfigure system gnome
+;;; $ sudo guix-reconfigure system gnome
 
 (define-module (trev-guix files scripts reconfigure)
   #:use-module (ice-9 match)
   #:use-module (guix scripts system)
-  #:use-module (guix scripts home))
+  #:use-module (guix scripts home)
+  #:use-module (guix build utils)
+  #:use-module (guix store)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-11))
 
 (define parse-home
   (match-lambda
@@ -26,14 +30,18 @@ exec guix repl -L "/home/trev/Workspace/dotfiles" -- "$0" "$@"
 
 (define parse-command
   (match-lambda
-    ((_ "home" . rest) (list guix-home parse-home rest))
-    ((_ "system" . rest) (list guix-system parse-system rest))
+    (("home" flavor) (list guix-home (parse-home flavor)))
+    (("system" flavor) (list guix-system (parse-system flavor)))
     (_ (error "Expected \"system\" or \"home\"\n"))))
 
-(define-public (reconfigure args)
-  (match (parse-command args)
-    ((fn parser rest)
-     (fn "reconfigure" (format #f "-e ~a" (parser (car rest)))))
-    (_ #f)))
+(define-public (reconfigure)
+  (let*-values (((args) (cdr (command-line)))
+               ((command rest) (split-at args 2)))
+    (match (parse-command command)
+      ((fn env)
+       (when rest
+         (setenv "GUIX_BUILD_OPTIONS" (string-join rest " ")))
+       (fn "reconfigure" (format #f "-e ~a" env)))
+      (_ #f))))
 
-(reconfigure (command-line))
+(reconfigure)
