57d474838890d25d338895a6aba2fd5c18b33851
Trevor Arjeski <tmarjeski@gmail.com>
2026-03-16 17:40:34 +0000

    [guix] bump emacs

--- trev-guix/packages/emacs.scm
+++ trev-guix/packages/emacs.scm
@@ -5,52 +5,8 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages emacs))
 
-(define* (emacs-ert-selector excluded-tests #:key run-nativecomp run-expensive run-unstable)
-  "Create an ERT selector that excludes tests."
-  (string-append
-   "(not (or "
-   (if run-nativecomp
-       ""
-       "(tag :nativecomp) ")
-   (if run-expensive
-       ""
-       "(tag :expensive-test) ")
-   (if run-unstable
-       ""
-       "(tag :unstable) ")
-   (string-join
-    (map
-     (lambda (test)
-       (string-append "\\\"" test "\\\""))
-     excluded-tests))
-   "))"))
-
-(define %selector
-  (emacs-ert-selector
-   '("bytecomp--fun-value-as-head"
-     "esh-util-test/path/get-remote"
-     "esh-var-test/path-var/preserve-across-hosts"
-     "ffap-tests--c-path"
-     "find-func-tests--locate-macro-generated-symbols"
-     "grep-tests--rgrep-abbreviate-properties-darwin"
-     "grep-tests--rgrep-abbreviate-properties-gnu-linux"
-     "grep-tests--rgrep-abbreviate-properties-windows-nt-dos-semantics"
-     "grep-tests--rgrep-abbreviate-properties-windows-nt-sh-semantics"
-     "info-xref-test-makeinfo"
-     "man-tests-find-header-file"
-     "tab-bar-tests-quit-restore-window"
-     "tramp-test50-remote-load-path"
-     "python-shell--convert-file-name-to-send-1"
-     "package-vc-tests"
-     "xref-elisp-test-find-defs-defgeneric-implicit-generic"
-     "pcase-tests-quote-optimization"
-     "test-map"
-     "process-test-stderr-buffer")))
-
-(module-set! (resolve-module '(gnu packages emacs)) '%selector %selector)
-
 (define-public emacs-next-next-pgtk
-  (let* ((commit "588d95e91dfa1b374481636608e94d70f8a12758")
+  (let* ((commit "1bae55cdbfed5b8a8b5a289edb42aac229f02f95")
          (version (git-version "31.0.50" "1" commit)))
     (package
       (inherit emacs-next-pgtk)
@@ -64,12 +20,11 @@
                       (commit commit)))
                 (file-name (git-file-name "emacs-next-next-pgtk" commit))
                 (sha256
-                 (base32 "101zg4n7n3yfl7ml0psbsgynxj6pzvfha4r4s82n3nkvp6b4chyz"))
+                 (base32 "1xkabypvmqd44y36nzrydjfs8s43j5fhbngav06xb0hlm7wqakxg"))
                 (patches
                  (search-patches "emacs-next-disable-jit-compilation.patch"
                                  "emacs-next-exec-path.patch"
                                  "emacs-fix-scheme-indent-function.patch"
                                  "emacs-native-comp-driver-options.patch"
                                  "emacs-next-native-comp-fix-filenames.patch"
-                                 "emacs-native-comp-pin-packages.patch"
-                                 "emacs-pgtk-super-key-fix.patch")))))))
+                                 "emacs-native-comp-pin-packages.patch")))))))
