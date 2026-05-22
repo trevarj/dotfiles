fde82d0360d08117e83898b6989b8dc92976c2f0
Trevor Arjeski <tmarjeski@gmail.com>
2026-05-22 14:43:07 +0000

    [guix] bump codex

--- trev-guix/packages/misc.scm
+++ trev-guix/packages/misc.scm
@@ -216,14 +216,14 @@ auto-updates for reproducibility and bundles fzf and ripgrep in PATH.")
 (define-public codex
   (package
     (name "codex")
-    (version "0.132.0")
+    (version "0.133.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append
              "https://github.com/openai/codex/releases/download/rust-v"
              version "/codex-x86_64-unknown-linux-musl.tar.gz"))
-       (sha256 (base32 "1cl3m36ia40fkiv66haf7wi8bg3nl8slbbbs35yisnzgwhp46r4b"))))
+       (sha256 (base32 "1vrl03dyv473mwmlazxz27m9gfy2anpb5sy2invq3limkjmijq6h"))))
     (build-system binary-build-system)
     (propagated-inputs (list bubblewrap))
     (arguments
